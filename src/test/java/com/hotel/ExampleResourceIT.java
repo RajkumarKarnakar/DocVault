b3f8c6c88a10a31e110bd31c8d8356727af3ae5a
planetofthecode <rajkumar.karnakar@outlook.com>
2024-07-24 21:56:36 +0530

    app initial setup

--- src/test/java/com/hotel/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.hotel;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
